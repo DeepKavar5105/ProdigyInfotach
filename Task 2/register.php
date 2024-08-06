4d803a06e44f025ddd149a27b80985fe08b1ddc5
DeepKavar5105 <dkkavar7@gmail.com>
2024-08-06 21:30:38 +0530

    Create register.php

---
+++ Task 2/register.php
@@ -0,0 +1,19 @@
+<?php
+require 'config.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+    $stmt = $pdo->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+    $stmt->execute([$username, $password]);
+
+    echo "Registration successful. <a href='login.php'>Login</a>";
+}
+?>
+
+<form method="POST" action="register.php">
+    <input type="text" name="username" required placeholder="Username">
+    <input type="password" name="password" required placeholder="Password">
+    <button type="submit">Register</button>
+</form>
