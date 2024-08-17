e12e567005569e61d9be47533b20864af2fe70a6
DeepKavar5105 <dkkavar7@gmail.com>
2024-08-17 17:58:57 +0530

    Add files via upload

---
+++ Task 4/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h2>Welcome to UsChat!</h2>
+      <p>This is the home page.</p>
+    </div>
+  );
+}
+
+export default HomePage;
